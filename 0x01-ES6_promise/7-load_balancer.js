8440231c41aeba796c94da3f6e5a22ac1e4ca488
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-11-29 15:34:34 +0000

    Added the loadbalancer testing functionlaity usin promise

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,5 +1,5 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  const promises = Promise.race([chinaDownload(), USDownload()]);
+  const promises = Promise.race([chinaDownload, USDownload]);
   return promises
     .then((res) => res.value);
 }
