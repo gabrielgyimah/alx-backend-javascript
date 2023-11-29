6628d253b77d88717d9c892c5b58440273cca074
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-11-29 15:25:19 +0000

    Added the handleProfileSignup function

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-
-  return promises
-    .then((results) => results.map((obj) => ({
-      status: obj.status,
-      value: obj.status === 'fulfilled' ? obj.value : obj.reason,
-    })));
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promise.then((res) => res.map((obj) => ({
+    status: obj.status, value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+  })));
 }
