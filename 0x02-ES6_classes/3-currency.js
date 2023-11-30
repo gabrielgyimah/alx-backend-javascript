d878de9f86c1b5467d027f7d097d6d8296686a5e
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-11-30 21:16:02 +0000

    Added the Currenncy class

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,25 +1,21 @@
 /* eslint-disable */
 
-class Currency {
+export default class Currency {
   constructor(code, name) {
-    this._validateString(code);
-    this._validateString(name);
     this._code = code;
     this._name = name;
   }
 
-  // getters
   get code() {
     return this._code;
   }
 
   get name() {
-    return this._name; // Added the missing return statement
+    return this._name;
   }
 
-  // setters
-  set code(code) {
-    this._code = code;
+  set code(newCode) {
+    this._code = newCode;
   }
 
   set name(name) {
@@ -27,12 +23,6 @@ class Currency {
   }
 
   displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
-
-  _validateString(property) {
-    if (typeof property !== 'string') { // Corrected the parameter name to be consistent
-      throw TypeError(`${property} must be a string`);
-    }
+    return `${this.name} (${this.code})`;
   }
 }
