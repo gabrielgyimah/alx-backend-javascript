8d5d9c143606393e4dc99f96c8ed105dd4c97eea
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-12-04 10:56:35 +0000

    Created the Data Manipulation project directory, added a README file and the getListStudents function which returns a lits of student objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+  const std1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const std2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const std3 = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [std1, std2, std3];
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,9 @@
+<h1>ES6 - Data Manipulation</h1>
+<h3>ALX Backend JavaScript</h3>
+<p>At the end of this project, you are expected to be able to explain to anyone, without the help of Google:</p>
+
+<ul>
+    <li>How to use map, filter and reduce on arrays</li>
+    <li>Typed arrays</li>
+    <li>The Set, Map, and Weak link data structures</li>
+</ul>
